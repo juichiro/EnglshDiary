ed792de6244b976babb279fea4559a1585258a15
EC2 Default User <ec2-user@ip-172-31-36-8.ap-northeast-1.compute.internal>
2019-11-11 08:24:05 +0000

    deviseで認証機能の実装

--- app/controllers/diaries_controller.rb
+++ app/controllers/diaries_controller.rb
@@ -1,8 +1,8 @@
 class DiariesController < ApplicationController
+  before_action :authenticate_user!
   def index
     @diaries = Diary.all
-  end
-
+  end 
   def show
     @diary = Diary.find(params[:id])
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_11_071706) do
+ActiveRecord::Schema.define(version: 2019_11_11_075756) do
 
   create_table "diaries", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.date "day"
@@ -18,6 +18,8 @@ ActiveRecord::Schema.define(version: 2019_11_11_071706) do
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_diaries_on_user_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
