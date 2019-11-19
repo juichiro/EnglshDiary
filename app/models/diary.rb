d0a2e3d461e9f19872cc7716a6998c12987ee0e6
EC2 Default User <ec2-user@ip-172-31-36-8.ap-northeast-1.compute.internal>
2019-11-19 08:39:44 +0000

    validationの追加

--- app/models/diary.rb
+++ app/models/diary.rb
@@ -1,3 +1,6 @@
 class Diary < ApplicationRecord
     belongs_to :user
+    validates :day, presence: true
+    validates :title, presence: true ,length: { maximum: 30 }
+    validates :description, presence: true 
 end
