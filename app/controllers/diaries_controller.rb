6ae29a27e39e361527b0c6a690a37343bd9e2829
EC2 Default User <ec2-user@ip-172-31-36-8.ap-northeast-1.compute.internal>
2019-11-26 07:13:28 +0000

    flashで通知をできるようにした

--- app/controllers/diaries_controller.rb
+++ app/controllers/diaries_controller.rb
@@ -17,19 +17,23 @@ class DiariesController < ApplicationController
   def create
     @diary = current_user.diaries.new(diary_params)
     if @diary.save
+      flash[:notice] = '正常に作成されました。'
       redirect_to diaries_url
     else
+      flash.now[:notice] = '作成に失敗しました。'
       render :new
     end
   end
   def update
     @diary = current_user.diaries.find(params[:id])
     @diary.update(diary_params)
+    flash[:notice] = '正常に更新されました。'
     redirect_to @diary
   end 
   def destroy
     @diary = current_user.diaries.find(params[:id])
     @diary.destroy
+    flash[:notice] = '正常に削除されました。'
     redirect_to diaries_url
   end 
   def show_translated_text
