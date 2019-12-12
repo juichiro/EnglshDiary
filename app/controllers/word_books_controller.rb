916f54e2cd40adb4f118c53f34ba1ece72086710
EC2 Default User <ec2-user@ip-172-31-36-8.ap-northeast-1.compute.internal>
2019-12-12 08:58:46 +0000

    word book new

--- app/controllers/word_books_controller.rb
+++ app/controllers/word_books_controller.rb
@@ -1,7 +1,20 @@
 class WordBooksController < ApplicationController
-  def index
+  before_action :authenticate_user!
+  
+  def index 
+    @words = current_user.word_books.all
   end
 
   def new
+    @word = current_user.word_books.new
+  end
+  def create
+    @word = current_user.word.new(word_params)
+    @word.save
+  end
+  
+  private
+  def word_params
+    params.require(:word).permit(:eword, :jword)
   end
 end
