b7120f230b7df54d0b7238fbaad0103d10f35684
EC2 Default User <ec2-user@ip-172-31-36-8.ap-northeast-1.compute.internal>
2020-03-10 09:09:43 +0000

    validation for word_book

--- app/models/word_book.rb
+++ app/models/word_book.rb
@@ -1,3 +1,5 @@
 class WordBook < ApplicationRecord
     belongs_to :user
+    validates :eword, presence: true,length: { maximum: 30 }
+    validates :jword, presence: true,length: { maximum: 30 }
 end
